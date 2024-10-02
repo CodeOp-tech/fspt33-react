03a03c30601c12f13b3998b4b375cff5f748ca3b
surajverma2587 <surajverma2587@gmail.com>
2024-10-02 11:32:06 +0100

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,64 @@ import { Animals } from "./components/Animals";
 import { Banner } from "./components/Banner";
 
 export const App = () => {
-  const animals = [
+  const catAnimals = [
     {
       id: uuidv4(),
-      name: "Animal 1",
+      name: "Cat 1",
       breed: "Breed 1",
-      description: "This is animal 1 of breed 1.",
+      description: "This is cat 1 of breed 1.",
       imageUrl: "https://placehold.co/600x400",
     },
     {
       id: uuidv4(),
-      name: "Animal 2",
+      name: "Cat 2",
       breed: "Breed 2",
-      description: "This is animal 2 of breed 2.",
+      description: "This is cat 2 of breed 2.",
       imageUrl: "https://placehold.co/600x400",
     },
     {
       id: uuidv4(),
-      name: "Animal 3",
+      name: "Cat 3",
       breed: "Breed 3",
-      description: "This is animal 3 of breed 3.",
+      description: "This is cat 3 of breed 3.",
+      imageUrl: "https://placehold.co/600x400",
+    },
+  ];
+
+  const dogAnimals = [
+    {
+      id: uuidv4(),
+      name: "Dog 1",
+      breed: "Breed 1",
+      description: "This is dog 1 of breed 1.",
       imageUrl: "https://placehold.co/600x400",
     },
     {
       id: uuidv4(),
-      name: "Animal 4",
+      name: "Dog 2",
+      breed: "Breed 2",
+      description: "This is dog 2 of breed 2.",
+      imageUrl: "https://placehold.co/600x400",
+    },
+    {
+      id: uuidv4(),
+      name: "Dog 3",
+      breed: "Breed 3",
+      description: "This is dog 3 of breed 3.",
+      imageUrl: "https://placehold.co/600x400",
+    },
+    {
+      id: uuidv4(),
+      name: "Dog 4",
       breed: "Breed 4",
-      description: "This is animal 4 of breed 4.",
+      description: "This is dog 4 of breed 4.",
       imageUrl: "https://placehold.co/600x400",
     },
     {
       id: uuidv4(),
-      name: "Animal 5",
+      name: "Dog 5",
       breed: "Breed 5",
-      description: "This is animal 5 of breed 5.",
+      description: "This is dog 5 of breed 5.",
       imageUrl: "https://placehold.co/600x400",
     },
   ];
@@ -45,7 +69,7 @@ export const App = () => {
     <div>
       <Banner />
       <main className="container">
-        <Animals animals={animals} />
+        <Animals catAnimals={catAnimals} dogAnimals={dogAnimals} />
       </main>
     </div>
   );
